a75bed416afb240209494364ddecf2eea881e3ac
rainyGLC <2929939024@qq.com>
2025-03-17 22:20:16 +0800

    add detatil

--- src/views/detail/index.jsx
+++ src/views/detail/index.jsx
@@ -1,15 +1,18 @@
 import React, { memo } from 'react'
 import { useSelector } from 'react-redux'
+import { DetailWrapper } from './style'
+import DetailPictures from './c-cpns/detail-pictures'
+import DetailInfos from './c-cpns/detail-infos'
 
 const Detail = memo(() => {
-  const {detailInfo} = useSelector((state)=>({
-    detailInfo: state.detail.detailInfo,
-  }))
+ 
   return (
-    <div>
-      <h2>{detailInfo.name}</h2>
-    </div>
+    <DetailWrapper>
+      <DetailPictures />
+      <DetailInfos />
+    </DetailWrapper>
   )
 })
+
 
 export default Detail
